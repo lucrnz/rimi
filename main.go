b48869e654c9bf98cbedc58336b860c2002fce2f
Luc lucdev.net <lucie_linux@protonmail.com>
2022-10-30 03:53:18 -0300

    Accidental orange site

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"sync"
+	"syscall"
 
 	"github.com/gorilla/mux"
 )
+
+type BookmarkStore struct {
+	mu   sync.Mutex
+	data []Bookmark
+}
 
 type Bookmark struct {
 	Title string `json:"title"`
@@ -43,6 +54,26 @@ func main() {
 		StyleCSS:  readFileAsStringOrPanic("./static/style.css"),
 	}
 
+	store := BookmarkStore{
+		data: make([]Bookmark, 0),
+	}
+
+	store.mu.Lock()
+	if _, err := os.Stat("./data.json"); err == nil {
+		data, err := os.ReadFile("./data.json")
+		if err != nil {
+			panic(err)
+		}
+
+		err = json.Unmarshal(data, &store.data)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Printf("Loaded %v bookmarks.\n", len(store.data))
+	store.mu.Unlock()
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -60,5 +91,73 @@ func main() {
 		w.Write([]byte(staticFiles.StyleCSS))
 	}).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	router.HandleFunc("/api/bookmarks", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			msg := fmt.Sprintf("Cannot read request: %v", err)
+			fmt.Fprintf(os.Stderr, "%v", msg)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(msg))
+			return
+		}
+
+		var bookmark Bookmark
+		err = json.Unmarshal(data, &bookmark)
+		if err != nil {
+			msg := fmt.Sprintf("Cannot parse request: %v", err)
+			fmt.Fprintf(os.Stderr, "%v", msg)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(msg))
+			return
+		}
+
+		if len(bookmark.Title) > 0 && len(bookmark.URL) > 0 {
+			store.mu.Lock()
+			defer store.mu.Unlock()
+			store.data = append(store.data, bookmark)
+			w.WriteHeader(http.StatusCreated)
+		} else {
+			msg := "Invalid bookmark"
+			fmt.Fprintf(os.Stderr, "%v", msg)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(msg))
+			return
+		}
+	}).Methods("POST")
+
+	router.HandleFunc("/api/bookmarks", func(w http.ResponseWriter, r *http.Request) {
+		store.mu.Lock()
+		defer store.mu.Unlock()
+		data, err := json.Marshal(store.data)
+		if err != nil {
+			msg := fmt.Sprintf("Cannot serialize store: %v", err)
+			fmt.Fprintf(os.Stderr, "%v", msg)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(msg))
+			return
+		}
+		w.Header().Add("Content-Type", "text/json; charset=UTF-8")
+		w.Write(data)
+	}).Methods("GET")
+
+	cancelChan := make(chan os.Signal, 1)
+	// catch SIGETRM or SIGINTERRUPT
+	signal.Notify(cancelChan, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		fmt.Printf("Server listening on port %v", port)
+		log.Fatal(http.ListenAndServe(":"+port, router))
+	}()
+	sig := <-cancelChan
+	log.Printf("Caught signal %v\n", sig)
+	store.mu.Lock()
+	defer store.mu.Unlock()
+	data, err := json.MarshalIndent(store.data, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("./data.json", data, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
--- static/main.js
+++ static/main.js
@@ -2,38 +2,52 @@ function q(sel) {
     return document.querySelector(sel);
 }
 
-let modalIsVisible = false;
-
-function hideModal() {
-    if (modalIsVisible) {
-        q("#add-bookmark").style.display = "none";
-        q("#toggle-add-btn").style.display = "block";
-        modalIsVisible = false;
+function renderBookmark(bookmark) {
+    const { title, url } = bookmark;
+    if (typeof title === 'string' &&
+        typeof url === 'string' &&
+        title.length > 0 &&
+        url.length > 0) {
+        const a = document.createElement("a");
+        a.innerText = title;
+        a.href = url;
+        q("#bookmarks").appendChild(a);
     }
 }
 
-function showModal() {
-    if (! modalIsVisible) {
-        q("#add-bookmark").style.display = "flex";
-        q("#toggle-add-btn").style.display = "none";
-        q("#txt-title").value = "";
+async function loadBookmarks() {
+    const response = await fetch("/api/bookmarks");
+    const data = await response.json();
+    
+    if (Array.from(q("#bookmarks").children).length > 0) {
+        q("#bookmarks").innerHTML = ""; 
+    }
+
+    data.forEach(bookmark => renderBookmark(bookmark));
+}
+
+q("#add-btn").addEventListener('click', async function () {
+    const url = q("#txt-url").value;
+    const title = q("#txt-title").value;
+
+    if (url.length > 0 && title.length > 0) {
+        await fetch("/api/bookmarks", {
+            method: "POST",
+            cache: "no-cache",
+            headers: {
+                'Content-Type': "text/json"
+            },
+            body: JSON.stringify({
+                title,
+                url
+            })
+        })
+        await loadBookmarks();
         q("#txt-url").value = "";
-        modalIsVisible = true;
-    }
-}
-
-q("#toggle-add-btn").addEventListener('click', function () {
-    if (modalIsVisible) {
-        hideModal();
-    } else {
-        showModal();
+        q("#txt-title").value = "";
     }
 });
 
-q("#confirm-btn").addEventListener('click', function () {
-    hideModal();
-});
-
-q("#cancel-btn").addEventListener('click', function () {
-    hideModal();
-});
+(async () => {
+    await loadBookmarks();
+})();
